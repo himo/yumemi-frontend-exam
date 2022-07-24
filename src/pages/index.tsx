6b3de8756a8f9fb3e8356f29a65bc05f570e47d7
himo <oohiray2015@gmail.com>
2022-07-25 03:32:42 +0900

    add: チェックを入れた際のグラフ表示機能の追加
    onChangeでasyncの戻り値の型Promise<void>の関数を使用したかったためeslintのルールを変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 
@@ -10,6 +12,11 @@ const Home: NextPage = () => {
     result?: PrefecturesData[]
   }
 
+  type seriesHighcharts = {
+    name: string
+    data: number[]
+  }
+
   type PrefecturesData = {
     prefCode: number
     prefName: string
@@ -17,6 +24,10 @@ const Home: NextPage = () => {
     data?: { year: number; value: number }[]
   }
   const [prefectures, setPrefectures] = useState<PrefecturesData[]>([])
+  const [valuesForHighcharts, setValuesForHighcharts] = useState<
+    seriesHighcharts[]
+  >([])
+  const [y_AxisYears, setY_AxisYears] = useState<number[]>([])
 
   const isPrefecturesData = (data: unknown): data is PrefecturesData[] => {
     return typeof data === 'object'
@@ -39,16 +50,125 @@ const Home: NextPage = () => {
     void getAxiosPrefecturesData()
   }, [])
 
+  const getAxiosPrefecturesPopulation = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ): Promise<void> => {
+    const updatedPrefectures = await Promise.all(
+      prefectures.map(async (x) => {
+        if (e.currentTarget.value == String(x.prefCode)) {
+          if (e.target.checked) {
+            x.selected = true
+            if (!('data' in x)) {
+              const url =
+                'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode='.concat(
+                  String(x.prefCode),
+                )
+              const use = async () => {
+                if (typeof process.env.NEXT_PUBLIC_API_KEY === 'string') {
+                  const response2 = await axios.get(url, {
+                    headers: {
+                      'X-API-KEY': process.env.NEXT_PUBLIC_API_KEY,
+                    },
+                  })
+                  x.data = [...response2.data.result.data[0].data]
+                }
+              }
+              await use()
+            }
+          } else {
+            x.selected = false
+          }
+        }
+        return x
+      }),
+    ).catch()
+    renderHighCharts()
+    setPrefectures(updatedPrefectures)
+  }
+
+  const renderHighCharts = () => {
+    const x_AxisYears =
+      prefectures.find((x) => x.data)?.data?.map((x) => x.year) ?? []
+    setY_AxisYears(x_AxisYears)
+
+    const chartIntoValue = prefectures
+      .filter((x) => x.selected)
+      .map(
+        (x: PrefecturesData): seriesHighcharts => ({
+          name: x.prefName,
+          data: x.data?.map((x) => x.value) ?? [],
+        }),
+      )
+    setValuesForHighcharts(chartIntoValue)
+  }
+
+  const options = {
+    chart: {
+      polar: true,
+      type: 'line',
+    },
+
+    title: {
+      text: '総人口推移',
+    },
+
+    xAxis: {
+      categories: y_AxisYears,
+      tickmarkPlacement: 'on',
+      lineWidth: 0,
+      title: {
+        text: '年',
+      },
+    },
+
+    yAxis: {
+      title: {
+        text: '人口数',
+      },
+    },
+
+    legend: {
+      align: 'right',
+      verticalAlign: 'middle',
+      layout: 'vertical',
+    },
+
+    series: valuesForHighcharts,
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            legend: {
+              align: 'center',
+              verticalAlign: 'bottom',
+              layout: 'horizontal',
+            },
+          },
+        },
+      ],
+    },
+  }
+
   return (
     <>
       <div>
         {prefectures.map((prefecture) => (
           <>
-            <input type="checkbox" value={prefecture.prefCode} />
+            <input
+              type="checkbox"
+              value={prefecture.prefCode}
+              onChange={getAxiosPrefecturesPopulation}
+            />
             <label htmlFor={prefecture.prefName}>{prefecture.prefName}</label>
             <br />
           </>
         ))}
+      </div>
+      <div>
+        <HighchartsReact highcharts={Highcharts} options={options} />
       </div>
     </>
   )
